f34cde6cc211abe9393c8b876aa9ad2dde1590e5
Rashmi G R <rashmigr01@gmail.com>
2023-09-02 20:15:07 +0530

    deploy

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/rashmigr01/rashmigr01.github.io.git";
+const repoURL = "https://github.com/rashmigr01/rashmigr01.github.io";
 
 ghpages.publish(
   pathname,
